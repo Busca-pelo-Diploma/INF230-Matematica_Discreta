7d64e052efe28c77bf536f0d50b668488e09cdbd
Thiago Ferreira <thiagofp707@gmail.com>
2022-07-08 13:40:45 +0000

    Começando 5

---
+++ T1/5.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    int matrix[n][n]; 
+    cin >> n;
+
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 2; j++){
+            matrix[i][j] = 1;
+        }
+    }
+
+    for(int i = 2; i < n; i++){
+        for(j = 0; j < n; j++){
+        }
+    }
+}
