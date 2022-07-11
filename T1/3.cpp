83b3ecd750e326ed688a212c482c8962bd808750
Levi Martins <levigmartins@gmail.com>
2022-07-11 19:16:47 -0300

    Long int nas funções de fibonacci (ex 3 e 4)

--- T1/3.cpp
+++ T1/3.cpp
@@ -1,13 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void imprime(int fib, int n, int count) {
+void imprime(int64_t fib, int n, int count) {
     cout << n << " - " << fib << " (fibonacci nº " << count << ")" << endl;
 }
 
-int fibonacci(int x) {
-    if(x==1 || x==2) return 1;
-    return fibonacci(x-1) + fibonacci(x-2);
+long int fibonacci(int n) {
+    long int a = 0;
+    long int b = 1;
+    
+    while (n-- > 1) {
+        long int aux = a;
+        a = b;
+        b += aux;
+    }
+
+    return b;
 }
 
 void calculaFibonacciMod5(int n) {
@@ -15,7 +23,7 @@ void calculaFibonacciMod5(int n) {
     int count=1, count_mod_5=0;
 
     while(count<=n){
-        int fib = fibonacci(count);
+        long int fib = fibonacci(count);
         if(fib%5==0) {
             count_mod_5++;
             imprime(fib, count_mod_5, count);
@@ -26,7 +34,7 @@ void calculaFibonacciMod5(int n) {
 
 int main() {
     int n;
-    cout << "Digite um valor n e verá os n primeiros numeros de Fibonacci que são divisíveis por 5: ";
+    cout << "Digite um valor n e verá os números de Fibonacci que são divisíveis por 5 entre 1 e n: ";
     cin >> n;
     calculaFibonacciMod5(n);
     return 0;
