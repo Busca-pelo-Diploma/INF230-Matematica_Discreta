53f5efc19adfaa420d24197654cb080436f0d22a
Thiago Ferreira Peixoto <thiago.ferreira@dtidigital.com.br>
2022-07-15 07:43:47 -0300

    5 teste

--- T1/5.cpp
+++ T1/5.cpp
@@ -3,13 +3,13 @@
 
 using namespace std;
 
-unsigned long long int calculaTrianguloPascal(unsigned long long int n){
+unsigned long long int calculaTrianguloPascal(int n){
 
     unsigned long long int ctd=0;
     unsigned long long int matrix[4*n][4*n];
     
-    for (unsigned long long int i=0; i<=n*2; i++){
-        for (unsigned long long int j=0; j<=i; j++){
+    for (int i=0; i<=n*2; i++){
+        for (int j=0; j<=i; j++){
     
             if (i == 0 || j == 0){
                 ctd = 1;
@@ -39,17 +39,17 @@ int main(){
     
     bool isDivisible = 0;
 
-    for(unsigned long long int n = 0; n < 30; n++){
+    for(int n = 0; n < 30; n++){
         
         isDivisible = 0;
-        
+
         unsigned long long int result = calculaTrianguloPascal(n);
         
-        for(unsigned long long int j = 0; (j*j) <= result; j++){
+        for(unsigned long long int j = 0; j*j <= result; j++){
             
             if(isPrime(j)){
 
-                if(result % (j*j)  == 0){
+                if(result % j*j  == 0){
                     cout << "Para n = " << n << ",  divisivel." << endl;
                     isDivisible = 1;
                     break;
