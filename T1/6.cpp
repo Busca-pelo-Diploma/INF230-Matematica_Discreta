3ad355911d674c9099c1eb1186ccb55caa40c98f
Thiago Ferreira Peixoto <thiago.ferreira@dtidigital.com.br>
2022-07-15 07:57:58 -0300

    5 teste

--- T1/6.cpp
+++ T1/6.cpp
@@ -1,15 +1,16 @@
-#include <stdio.h>
 #include <bits/stdc++.h>
+#include <math.h>
 
 using namespace std;
 
-int calculaTrianguloPascal(int n){
-    int ctd=0;
-    int matrix[n][n];
+unsigned long long int calculaTrianguloPascal(unsigned long long int n){
+
+    unsigned long long int ctd=0;
+    unsigned long long int matrix[2*n][2*n];
     
-    for (int i=0; i<=n*2; i++){
-        for (int j=0; j<=i; j++){
-            
+    for (unsigned long long int i=0; i<=n; i++){
+        for (unsigned long long int j=0; j<=i; j++){
+    
             if (i == 0 || j == 0){
                 ctd = 1;
             }else{
@@ -21,22 +22,37 @@ int calculaTrianguloPascal(int n){
     return matrix[n][n/2];
 }
 
+bool isPrime(unsigned long long int n){
+    if (n == 1){
+        return false;
+    }
+    for (unsigned long long int i=2; i<n; i++){
+        if (n%i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
 int main(){
-    
-  int cont = 0;
-    
-  for(int n = 0; n < 200; n++){
-      
-      if(calculaTrianguloPascal(n) % 121  == 0){
-          cout << "Para n = " << n << ",  divisivel." << endl;
-          cont++;
-      }
-      
-  }
 
-  if(cont == 0){
-      cout << "Não tem nenhum entre 1 e 200." << endl;
-  }
+    
+    for(unsigned long long int n = 1; n <= 200; n+=2){
+        
+        bool isDivisible = 0;
 
-  return 0;
+        unsigned long long int result = calculaTrianguloPascal(n);
+
+        for(unsigned long long int j = 2; (j*j) <= result; j++){
+
+            if(isPrime(j) && (result % (j*j)  == 0)){
+                isDivisible = 1;
+                break;
+            }
+        }  
+        if(!isDivisible){
+            cout << n << " nao e divisivel." << endl;
+        }
+    }
+
 }
